ba20d0e0c5e0c274e80bb66439ee7b4a02b0c264
saikiranmerugu74 <saikiran.merugu74@gmail.com>
2024-05-02 09:22:25 -0400

    removed tf eks files

--- Terraformeks/backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket         = "eks-pyappbackend" # Replace with your actual S3 bucket name
-    key            = "EKS/terraform.tfstate"
-    region         = "us-east-1"
-    dynamodb_table = "my-terraform-state-bucket"
-    encrypt        = true
-  }
-}
